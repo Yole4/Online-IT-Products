6dff13bcf951086ff81825f7e0e0bf0a4c8b17ac
Eloy <shelomora60@gmail.com>
2023-10-31 07:24:12 +0800

    push

--- client/src/pages/body/Home.jsx
+++ client/src/pages/body/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../../assets/css/Home.css';
 
@@ -14,9 +14,15 @@ import { useNavigate } from 'react-router-dom';
 import { LuShoppingCart } from "react-icons/lu";
 import { BsPersonCircle } from "react-icons/bs";
 import { BiSearchAlt2 } from "react-icons/bi";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { VscDeviceCamera } from "react-icons/vsc";
 
 function Home() {
   const navigate = useNavigate();
+
+  const [isProfile, setIsProfile] = useState(false);
+  const [isCart, setIsCart] = useState(false);
+  const [isProductClick, setIsProductClick] = useState(false);
 
   return (
     <>
@@ -25,65 +31,33 @@ function Home() {
         <div>
           <BiSearchAlt2 style={{ position: 'absolute', right: '287px', zIndex: '50', marginTop: '25px', color: '#5c5c5c' }} size={23} />
           {/* <input type="text" style={{borderRadius: '10px', height: '36px', padding: '10px 10px 10px 30px'}}/> */}
-          <input type="text" className="form-control" style={{position: 'absolute', right: '120px', paddingLeft: '38px', width: '200px', top: '18px'}}/>
+          <input type="text" className="form-control" style={{ position: 'absolute', right: '120px', paddingLeft: '38px', width: '200px', top: '18px' }} />
 
-          <LuShoppingCart style={{ margin: '20px 10px 20px 20px', color: '#fff', cursor: 'pointer' }} size={30} />
-          <BsPersonCircle style={{ margin: '20px 20px 20px 10px', color: '#fff', cursor: 'pointer' }} size={30} />
+          <LuShoppingCart style={{ margin: '20px 10px 20px 20px', color: '#fff', cursor: 'pointer' }} onClick={() => setIsCart(isCart ? false : true)} size={30} />
+          <BsPersonCircle style={{ margin: '20px 20px 20px 10px', color: '#fff', cursor: 'pointer' }} onClick={() => setIsProfile(isProfile ? false : true)} size={30} />
         </div>
       </div>
 
       <div className="product-options">
-        <button className='active'>All</button>
-        <button>Mouse</button>
-        <button>Laptop</button>
-        <button>Computer</button>
-        <button>Computer</button>
+        <div className='product-list'>
+          <button className='active'>All</button>
+        </div>
+        <div className='product-list'>
+          <button>Mouse</button>
+        </div>
+        <div className='product-list'>
+          <button>Laptop</button>
+        </div>
+        <div className='product-list'>
+          <button>Computer</button>
+        </div>
+        <div className='product-list'>
+          <button>Laptop</button>
+        </div>
       </div>
 
       <div className="gallery">
-        {/* <div className="product-content">
-          <img src={mouse} className='product-image' alt />
-          <h3 className='product-name'>testing</h3>
-          <p className='product-description'>this is the sample image</p>
-          <h6 className='product-ammount'>₱100</h6>
-          <ul className='product-ul'>
-            <li className='product-li'><i className="fa fa-star checked star" /></li>
-            <li className='product-li'><i className="fa fa-star checked star" /></li>
-            <li className='product-li'><i className="fa fa-star checked star" /></li>
-            <li className='product-li'><i className="fa fa-star checked star" /></li>
-            <li className='product-li'><i className="fa fa-star star" /></li>
-          </ul>
-          <button className="buy-2">Buy Now</button>
-        </div>
-        <div className="product-content">
-          <img src={computer} className='product-image' alt />
-          <h3 className='product-name'>testing</h3>
-          <p className='product-description'>this is the sample image</p>
-          <h6 className='product-ammount'>₱100</h6>
-          <ul className='product-ul'>
-            <li className='product-li'><i className="fa fa-star checked star" /></li>
-            <li className='product-li'><i className="fa fa-star checked star" /></li>
-            <li className='product-li'><i className="fa fa-star checked star" /></li>
-            <li className='product-li'><i className="fa fa-star checked star" /></li>
-            <li className='product-li'><i className="fa fa-star star" /></li>
-          </ul>
-          <button className="buy-3">Buy Now</button>
-        </div>
-        <div className="product-content">
-          <img src={laptop} className='product-image' alt />
-          <h3 className='product-name'>testing</h3>
-          <p className='product-description'>this is the sample image</p>
-          <h6 className='product-ammount'>₱100</h6>
-          <ul className='product-ul'>
-            <li className='product-li'><i className="fa fa-star checked star" /></li>
-            <li className='product-li'><i className="fa fa-star checked star" /></li>
-            <li className='product-li'><i className="fa fa-star checked star" /></li>
-            <li className='product-li'><i className="fa fa-star checked star" /></li>
-            <li className='product-li'><i className="fa fa-star star" /></li>
-          </ul>
-          <button className="buy-4">Buy Now</button>
-        </div> */}
-        <div className="product-content">
+        <div className="product-content" onClick={() => setIsProductClick(isProductClick ? false : true)}>
           <img src={laptop} className='product-image' alt="" />
           <h3 className='product-name'>Laptop</h3>
           <div className="ammount" style={{ textAlign: 'left', marginLeft: '20px', color: 'red' }}>
@@ -243,6 +217,75 @@ function Home() {
           </div>
         </div>
       </div>
+
+
+      {/* --------   PROFILE ---------- */}
+      {isProfile && (
+        <div className="popup" onClick={() => setIsProfile(false)}>
+          <div className="popup-body" onClick={(e) => e.stopPropagation()} style={{ animation: isProfile ? 'dropBottom .3s linear' : '' }}>
+            <div className="modal-close" onClick={() => setIsProfile(false)}>
+              <AiOutlineCloseCircle size={30} />
+            </div>
+            <div style={{ textAlign: 'center' }}>
+              <img src={mouse} style={{ borderRadius: '50%', height: '150px', width: '150px' }} />
+              <label htmlFor="uploadPhoto" style={{ marginLeft: '-40px', cursor: 'pointer', zIndex: '3', color: 'white', position: 'absolute', marginTop: '110px' }}>
+                <VscDeviceCamera size={30} style={{ backgroundColor: 'rgb(71, 71, 98)', padding: '3px', borderRadius: '50%' }} />
+                <input type="file" id="uploadPhoto" style={{ display: 'none' }} />
+                {/* <input type="file" id="uploadPhoto" onChange={(e) => setAutoImage(e.target.files[0])} style={{ display: 'none' }} /> */}
+              </label>
+            </div>
+            <div style={{ textAlign: 'center' }}>
+              <div>
+                <h2 style={{ fontSize: '20px' }}>Shelo Mora Paglinawan</h2>
+              </div>
+              <div style={{ marginTop: '10px' }}>
+                <span>Customer</span>
+              </div><br />
+            </div>
+            <hr />
+            <div className="form-control" style={{ textAlign: 'center' }}>
+              <span>Other profile view</span>
+            </div>
+          </div>
+        </div>
+      )}
+
+
+      {/* --------   CART ---------- */}
+      {isCart && (
+        <div className="popup" onClick={() => setIsCart(false)}>
+          <div className="popup-body" onClick={(e) => e.stopPropagation()} style={{ animation: isCart ? 'dropBottom .3s linear' : '' }}>
+            <div className="modal-close" onClick={() => setIsCart(false)}>
+              <AiOutlineCloseCircle size={30} />
+            </div>
+            cart is empty
+          </div>
+        </div>
+      )}
+
+      {/* --------   PRODUCT LIST ---------- */}
+      {isProductClick && (
+        <div className="popup" onClick={() => setIsProductClick(false)}>
+          <div className="popup-body" onClick={(e) => e.stopPropagation()} style={{ animation: isProductClick ? 'dropBottom .3s linear' : '' }}>
+            <div className="modal-close" onClick={() => setIsProductClick(false)}>
+              <AiOutlineCloseCircle size={30} />
+            </div>
+            <div>
+              <span>Mouse</span>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+              <span>Stock: 40</span>
+              <div>
+                <span>Ammount: </span>
+                <span>₱40</span>
+              </div>
+            </div>
+            <div>
+              description
+            </div>
+          </div>
+        </div>
+      )}
     </>
   )
 }
